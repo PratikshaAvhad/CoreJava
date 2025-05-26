536a24e6cf10d73a9b7a9fb406c3d3820a6f405f
Pratiksha Avhad <pratikshaavhad91@gmail.com>
2025-05-26 18:45:39 +0530

    Add files via upload

---
+++ acc_disp.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class emp
+{
+   public static void main(String arg[])
+ {
+   Scanner sc=new Scanner(System.in);
+   System.out.println("enter emp no");
+   int eno=sc.nextInt();
+   System.out.println("enter emp name");
+   String ename=sc.next();
+   System.out.println("enter emp sal");
+   float sal=sc.nextFloat();
+
+   System.out.printf("\n emp no=%d",eno);
+    System.out.printf("\n emp name=%s",ename);
+     System.out.printf("\n emp salary=%f",sal);  
+  
+}
+}
+   
---
+++ add.java
@@ -0,0 +1,10 @@
+class demo
+{
+  public static void main(String arg[])
+{
+   int a=30;
+  int b=30;
+   int c=a+b;
+   System.out.printf("addition=%d",c);
+}
+}
