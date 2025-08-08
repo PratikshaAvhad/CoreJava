90681ef752d6b76f999a79661cb005fc4847d509
Pratiksha Avhad <pratikshaavhad91@gmail.com>
2025-08-08 23:36:06 +0530

    Add files via upload

---
+++ NegativeArraySize_Exception.java
@@ -0,0 +1,15 @@
+class Demo
+{
+  public static void main(String arg[])
+ {
+  try
+  {
+   int a[]=new int[-5];
+   System.out.println("Array Create");
+  }
+  catch(Exception e)
+  {
+    System.out.println("Error="+e);
+  }
+ }
+} 
