67f7bd36e7f5fb36d16ab88c4b94cd86ea447369
Pratiksha Avhad <pratikshaavhad91@gmail.com>
2025-08-08 23:37:49 +0530

    Add files via upload

---
+++ StringIndexOutofBound_Exception.java
@@ -0,0 +1,17 @@
+class Demo
+{
+  public static void main(String arg[])
+ {
+  try
+  {
+   String s="cdj";
+   System.out.println("string="+s);
+   System.out.println("fisrt character="+s.charAt(0));
+   System.out.println("character="+s.charAt(7));
+  }
+  catch(Exception e)
+  {
+    System.out.println("Error="+e);
+  }
+ }
+} 
