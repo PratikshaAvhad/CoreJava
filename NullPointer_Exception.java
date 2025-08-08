8362a9811b3f85d1ac8e4a863ded92a2afa0338b
Pratiksha Avhad <pratikshaavhad91@gmail.com>
2025-08-08 23:36:35 +0530

    Add files via upload

---
+++ NullPointer_Exception.java
@@ -0,0 +1,15 @@
+class Demo
+{
+  public static void main(String arg[])
+ {
+  try
+  {
+    String s1=null;
+    System.out.println("length="+s1.length());
+  }
+  catch(Exception e)
+  {
+    System.out.println("Error="+e);
+  }
+ }
+} 
