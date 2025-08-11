890b6480f5e56b39e4cb26ac24c1e380854f2bde
Pratiksha Avhad <pratikshaavhad91@gmail.com>
2025-08-11 23:56:37 +0530

    Add files via upload

---
+++ content_reverse_file.java
@@ -0,0 +1,24 @@
+import java.io.*;
+class Demo
+{
+ public static void main(String arg[])
+ {
+  try
+  {
+    FileReader f1=new FileReader("abc.txt");
+    BufferedReader br=new BufferedReader(f1);
+     String s="";
+    StringBuffer sbr=new StringBuffer();
+    while((s=br.readLine())!=null)
+   {
+     sbr.append(s+"\n");
+   }
+     sbr.reverse();
+     System.out.printf("%s",sbr);
+ }
+ catch(Exception e)
+ {
+   System.out.println("error="+e);
+ }
+}
+} 
